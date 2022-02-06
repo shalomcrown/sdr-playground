c628f44a240f961a6805d2279572e885dd57b85c
Shalom Crown <shalomc@airobotics.co.il>
2022-02-06 14:10:40 +0200

    Logparser: Fix master/slave - sometimes no mission ID

--- sdr.py
+++ sdr.py
@@ -17,13 +17,19 @@ def plot(freq):
     sdr.gain = 4
 
     samples = sdr.read_samples(256*1024)
+
+    # Attempt to calculate DC offset using rolling average
     offsets = np.convolve(np.real(samples), np.ones(1024), 'valid') / 1024
+    # Pad the offsets array to size of samples (needs first 1024 members)
     offsets = np.pad(offsets, (len(samples) - len(offsets), 0), mode='constant', constant_values=offsets[0])
+    # Subtract 'DC" offsets from samples.
     samples = samples - offsets.astype(complex)
 
+    # Other method of removing DC component using mean of entire sample array
     #offsets = np.mean(np.real(samples)
     #samples = samples - complex(np.mean(np.real(samples)), 0)
 
+    # Various attempts to calculate PSD
     # PSD = (np.abs(np.fft.fft(samples))/len(samples))**2
     # PSD_log = 10.0 * np.log10(PSD)
     # PSD_shifted = np.fft.fftshift(PSD_log)
@@ -33,14 +39,21 @@ def plot(freq):
     # plt.show()
 
     # psdsk,freqs = ss.my_psd(samples, NFFT=1024, Fs=sdr.sample_rate)
-    #
     # plt.plot(freqs + sdr.center_freq, psdsk)
     # plt.show()
 
     # use matplotlib to estimate and plot the PSD
-    plt.psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+    psd_values, frequencies = plt.psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+    PSD_log = 10.0 * np.log10(psd_values)
+    PSD_shifted = np.fft.fftshift(PSD_log)
+
+    PSD_min = np.min(PSD_shifted)
+    PSD_max = np.max(PSD_shifted)
+
     plt.xlabel('Frequency (MHz)')
     plt.ylabel('Relative power (dB)')
+    plt.yticks(np.arange(int(PSD_min), int(PSD_max), 2))
+
     return
 
 
@@ -56,6 +69,6 @@ while True:
     plt.subplot(1, 3, 3)
     plot(1227.60)
     fig.canvas.draw()
-    time.sleep(0.4)
+    time.sleep(0.2)
     fig.clear()
 
